4cf3461be7b0d5c93ca94d34afd48b4da735f9bd
jovannakov <jovan.nakov@students.finki.ukim.mk>
2019-02-16 20:45:20 +0100

    Completed the class that represents the block.

--- js_files/block.js
+++ js_files/block.js
@@ -6,4 +6,14 @@ class Block {
         this.data = data;
     }
 
+    toString(){
+        // We are returning a template string, it is similar with the basic string, except we can print it in multiple lines
+        // and we can use
+        return `Block - 
+            Timestamp : ${this.timestamp}
+            Last hash : ${this.lastHash.substring(0, 10)}
+            Hash      : ${this.hashValue.substring(0, 10)}
+            Data      : ${this.data}`;
+    }
+
 }
