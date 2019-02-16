229542de616d328d4f01269cc8bd955ac9885939
jovannakov <jovan.nakov@students.finki.ukim.mk>
2019-02-16 20:31:02 +0100

    Created the class that represents blocks

---
+++ js_files/block.js
@@ -0,0 +1,9 @@
+class Block {
+    constructor(timestamp, lastHash, hashValue, data) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hashValue = hashValue;
+        this.data = data;
+    }
+
+}
