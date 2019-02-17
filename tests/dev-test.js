6504007aeb48da41dd4185fb9a055123d847e7ea
jovannakov <jovan.nakov@students.finki.ukim.mk>
2019-02-17 16:07:26 +0100

    Tested the chain with 10 blocks

--- tests/dev-test.js
+++ tests/dev-test.js
@@ -1,4 +1,11 @@
 const  Block = require("../js_files/block");
 
-const block = Block.mineBlock(Block.genesis().hashValue, "First mined block");
+var block = Block.mineBlock(Block.genesis(), "First mined block");
 console.log(block.toString());
+var data = [];
+for (var i = 0; i < 10; i++){
+    data.push(i);
+    var newBlock = Block.mineBlock(block, data);
+    console.log(newBlock.toString());
+    block = newBlock;
+}
