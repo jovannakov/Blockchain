425ba848415ff5e59031edcdd9259ba906bddaf9
jovannakov <jovan.nakov@students.finki.ukim.mk>
2019-02-24 01:40:47 +0100

    Created and tested the P2P server

--- app/index.js
+++ app/index.js
@@ -1,27 +1,22 @@
 const express = require("express");
 const Blockchain = require("../blockchain/index");
 const bodyParser = require("body-parser");
-const path = require("path");
-const http = require('http');
+const P2pServer = require("./p2p-server");
 
-const HTTP_PORT = process.env.PORT || 3001;
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
-const blockchain = new Blockchain();
+const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
-
-
-
-
 app.get('/blocks', (req, res)=>{
-    res.json(blockchain.chain);
+    res.json(bc.chain);
 });
 
-
 app.post('/mine', (req, res) => {
-   const block = blockchain.addBlock(req.body.data);
+   const block = bc.addBlock(req.body.data);
    console.log(`New block added ${block.toString()}`);
    res.redirect('/blocks');
 });
@@ -29,3 +24,5 @@ app.post('/mine', (req, res) => {
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
 });
+
+p2pServer.listen();
